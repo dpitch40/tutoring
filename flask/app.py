37560bf2f884aa28b3326a19bfe0ee291c553103
David Pitchford <david.t.pitchford@gmail.com>
2021-10-14 15:35:05 -0500

    Added flask module

---
+++ flask/app.py
@@ -0,0 +1,38 @@
+from contextlib import contextmanager
+
+from flask import Flask
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_URL = f"sqlite:///test.db"
+engine = create_engine(DB_URL)
+session = sessionmaker(bind=engine)
+@contextmanager
+def session_scope(self, *args, **kwargs):
+    session = session(*args, **kwargs)
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+Base = declarative_base()
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    id_ = Column("id", Integer, primary_key=True)
+    speed = Column(Integer)
+    color = Column(String)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
