3f53024f05dd690e2b248eb3d7960d1ac518e3c1
David Pitchford <david.t.pitchford@gmail.com>
2022-01-31 15:46:54 -0600

    Filled out first few mathstr unit tests

--- python/mathstr_tests.py
+++ python/mathstr_tests.py
@@ -8,11 +8,28 @@ def mathstr1():
 def mathstr2():
     return MathStr("abc")
 
+@pytest.fixture(scope="function")
+def mathstr3():
+    return MathStr("abcdef")
+
 
 # Binary operator tests
 
-def test_int_addition(mathstr2):
-    assert mathstr2 + 1 == 
+def test_str_addition(mathstr1):
+    assert mathstr1 + "abc" == MathStr("aabc")
 
-def test_str_addition(mathstr2):
-    assert mathstr2 + "abc" == MathStr("aabc")
+def test_str_subtraction(mathstr1, mathstr2, mathstr2):
+    assert MathStr("abc") - "c" == MathStr("ab")
+    assert MathStr("aabc") - "abc" == mathstr1
+    assert mathstr2 - mathstr1 + mathstr1 == mathstr2
+    assert mathstr2 - mathstr1 == MathStr(["a", "b", "c", -MathStr("a")])
+
+    assert mathstr3 - mathstr2 == MathStr(["abcdef", -MathStr("abc")])
+    assert mathstr3 - mathstr2 + mathstr2 == mathstr3
+
+
+# Unary operator tests
+
+def test_str_negation(mathstr2):
+    assert -mathstr2 == MathStr([-MathStr("a"), -MathStr("b"), -MathStr("c")])
+    assert -(-mathstr2) == mathstr2
