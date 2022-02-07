8d1bd0e550cc7fd831e83dadf9aa1e4e76e24d97
David Pitchford <david.t.pitchford@gmail.com>
2022-02-07 14:16:31 -0600

    Added new exercise

---
+++ python/filepath_cleanup.py
@@ -0,0 +1,40 @@
+import pytest
+import random
+
+def filter_filename(fname):
+    raise NotImplementedError
+
+
+def test_filter_out_illegal_characters():
+    assert filter_filename('/home/tuser/normal.txt') == '/home/tuser/normal.txt'
+    assert filter_filename('/home/tuser/:colon.txt') == '/home/tuser/_colon.txt'
+    assert filter_filename('/home/tuser/;semicolon.txt') == '/home/tuser/_semicolon.txt'
+    assert filter_filename('/home/tuser/\\backslash.txt') == '/home/tuser/_backslash.txt'
+    assert filter_filename('/home/tuser/!bang.txt') == '/home/tuser/_bang.txt'
+    assert filter_filename('/home/tuser/?interro.txt') == '/home/tuser/_interro.txt'
+    assert filter_filename('/home/tuser/*asterisk.txt') == '/home/tuser/_asterisk.txt'
+    assert filter_filename('/home/tuser/"quote.txt') == '/home/tuser/_quote.txt'
+    assert filter_filename('/home/tuser/<lt.txt') == '/home/tuser/_lt.txt'
+    assert filter_filename('/home/tuser/>gt.txt') == '/home/tuser/_gt.txt'
+    assert filter_filename('/home/tuser/|vbar.txt') == '/home/tuser/_vbar.txt'
+    assert filter_filename('/home/tuser/.txt') == '/home/tuser/_vbar.txt'
+
+def test_filter_out_non_ascii_characters():
+    for i in range(10):
+        fname_template = '/home/tuser/%snon_ascii.txt'
+        assert filter_filename(fname_template % chr(random.randint(256, 0x1000)),
+                               fname_template % '_')
+
+def test_filter_initial_period():
+    assert filter_filename('/home/tuser/Music/...OK Go/01 Song.ogg',
+        '/home/tuser/Music/_..OK Go/01 Song.ogg')
+
+def test_filter_end_period():
+    assert filter_filename('/home/tuser/Music/OK Go/01 No_ext.',
+        '/home/tuser/Music/OK Go/01 No_ext_')
+
+def test_filter_end_space():
+    assert filter_filename('/home/tuser/Documents/ stuff/doc.txt',
+        '/home/tuser/Documents/ stuff/doc.txt')
+    assert filter_filename('/home/tuser/Documents/stuff /doc.txt',
+        '/home/tuser/Documents/stuff_/doc.txt')
